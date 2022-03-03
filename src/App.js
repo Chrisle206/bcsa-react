a1758321e9ab797c7536c86d5b3d3576af287682
Christopher Le <Chrisle808@yahoo.com>
2022-03-03 11:36:51 -0800

    changed App.js added style.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./style.css"
+import Start from "./pages/Start";
+import Tavern from "./pages/Tavern";
+import Character from "./pages/Character";
+import Creation from "./pages/Creation";
+import Dead from "./pages/Dead";
+import Loading from "./pages/Loading";
+import Battle from "./pages/Battle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Link className="link" to="/">Start</Link>
+        <Link className="link" to="/Tavern">Tavern page</Link>
+        <Link className="link" to="/Character">Character page</Link>
+        <Link className="link" to="/Creation">Creation page</Link>
+        <Link className="link" to="/Dead">Dead page</Link>
+        <Link className="link" to="/Loading">Loading page</Link>
+        <Link className="link" to="/Battle">Battle page</Link>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/Tavern" element={<Tavern />} />
+          <Route path="/Character" element={<Character />} />
+          <Route path="/Creation" element={<Creation />} />
+          <Route path="/Dead" element={<Dead />} />
+          <Route path="/Loading" element={<Loading />} />
+          <Route path="/Battle" element={<Battle />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
