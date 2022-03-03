7376d993ba7c4b0952db5b8eadc5ab79f98577cd
velkyam <velkyam@hotmail.com>
2022-03-03 13:46:03 -0800

    Tavern

--- src/pages/Tavern.js
+++ src/pages/Tavern.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../App.css'
+import coins from '../assets/images/coins.png'
+import book from '../assets/images/spell-book-white.png'
+import charIcon from '../assets/images/mona-lisa-white.png'
+import cage from '../assets/images/cage-white.png'
+import beer from '../assets/images/beer-stein-white.png'
 
 export default function Tavern() {
     return (
-        <div>
-            <h1>This is the Tavern page!</h1>
+        <div className="pageContainer">
+        <div className="mainContainer">    
+        <h3 className="coinContainer"> <img className="coinImg" src={coins} alt="coins"/> 200</h3>
+        <h1 className="pageTitle">TAVERN<img className="TavernBeer" src={beer}/></h1>
+        <div className="TavernMenuContainer">    
+            <div className="TavernMenuCard"><img className="TavernMenuItem" src={charIcon}/>Character</div>
+            <div className="TavernMenuCard"><img className="TavernMenuItem" src={book}/>Story</div>
+            <div className="TavernMenuCard"><img className="TavernMenuItem" src={cage}/>Dungeon</div>
+        </div>
+        </div>
         </div>
     )
 }
