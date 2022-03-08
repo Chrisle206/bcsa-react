dafd70b11d13467b44af3fd7256444ea3c51c960
Christopher Le <Chrisle808@yahoo.com>
2022-03-07 19:47:18 -0800

    added battle sim

--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -1,31 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react';
 import enemyPic from '../assets/images/enemy.png'
 import heroPic from '../assets/images/hero.png'
 
 export default function Battle() {
+
+
+class Character {
+    constructor(attacks, name, hp, atk, def) {
+      this.attacks = attacks;
+      this.name = name;
+      this.hp = hp;
+      this.atk = atk;
+      this.def = def;
+    }
+  
+    // Method which prints all of the stats for a character
+    printStats() {
+      console.log(`${this.name}'s current hp: ${this.hp}`);
+    }
+  
+    // Method which determines whether or not a character's "hp" are less then zero
+    // Returns true or false depending upon the outcome
+    isAlive() {
+      if (this.hp <= 0) {
+        console.log(`${this.name} has been defeated!`);
+        return false;
+      }
+      return true;
+    }
+  
+    // Method which takes in a second object and decreases their "hp" by this character's atk
+    attack(opponent) {
+      console.log(`${this.name} used ${this.attacks[Math.floor(Math.random() * this.attacks.length)]} on ${opponent.name} for ${this.atk} damage`);
+      opponent.hp = (opponent.hp + opponent.def) - this.atk;
+    }
+    
+  }
+
+
+  // Create unique characters using the "character" constructor
+  const opponent = new Character(["Room Mute",
+  "Random Name Selector",
+  "Confusing Demo",
+  "Manatee Joke",
+  "Bahamut Bash",
+  "Shiva Shank",
+  "La Croix Heal"],
+  'Boss1',
+  100,
+  55,
+  20
+  );
+
+  const player = new Character(['Attack 1', 'Attack 2', 'Attack 3', 'Attack 4'], 'player', 150, 45, 15);
+  
+  
+  
     return (
-        // <div>
-        //     <div>
-        //     <button>Move 1</button>
-        //     <button>Attack 2</button>
-        //     <button>Attack 3</button>
-        //     <button>Heal</button>
-        //     </div>
-        //     <p>Insert Boss Text</p>
-        // </div>
         <div className="pageContainer creationBg">
             <div className="MainBattleContainer">
                 <div className="battleContainer">
                     <div className="enemyRow">
                         <div className="StatBox">
                             <div className='statRow'>
-                                <h3>Boss Name</h3>
+                                <h3>{opponent.name}</h3>
                                 <h3>Lvl: 15</h3>
                             </div>
                             <div className='healthBarContainer'>
                                 <div className='statRow'>
                                 <div className='healthBar50'></div>
-                                <h3 className='hp'>HP:50</h3>
+                                <h3 className='hp'>HP:{opponent.hp}</h3>
                                 </div>
                             </div>
                         </div>
@@ -35,13 +79,13 @@ export default function Battle() {
                         <img className="heroPic" src={heroPic} alt="Hero" />
                         <div className="StatBox">
                             <div className='statRow'>
-                                <h3>Username</h3>
+                                <h3>{player.name}</h3>
                                 <h3>Lvl: 10</h3>
                             </div>
                             <div className='healthBarContainer'>
                                 <div className='statRow'>
                                 <div className='healthBar100'></div>
-                                <h3 className='hp'>HP:100</h3>
+                                <h3 className='hp'>HP:{player.hp}</h3>
                                 </div>
                             </div>
                         </div>
@@ -50,12 +94,12 @@ export default function Battle() {
                 <div className="BattlechoicesContainer">
                     <div className="attackList">
                         <div className="attackRow">
-                            <div>Move 1</div>
-                            <div>Move 2</div>
+                            <div onClick={() => player.attack(opponent)}>{player.attacks[0]}</div>
+                            <div onClick={() => player.attack(opponent)}>{player.attacks[1]}</div>
                         </div>
                         <div className="attackRow">
-                            <div>Move 3</div>
-                            <div>Move 4</div>
+                            <div onClick={() => player.attack(opponent)}>{player.attacks[2]}</div>
+                            <div onClick={() => player.attack(opponent)}>{player.attacks[3]}</div>
                         </div>
                     </div>
                 </div>
