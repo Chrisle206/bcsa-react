329e9cdb15b94d83c5aa1a10de9170a55d6b9c1f
Christopher Le <Chrisle808@yahoo.com>
2022-03-13 14:20:47 -0700

    testing store logic

--- src/Javascript/getCharacter.js
+++ src/Javascript/getCharacter.js
@@ -1,36 +1,38 @@
-const getCharacter = async ()=>{
-  const token = localStorage.getItem("token");
-  const characterId = localStorage.getItem("characterId");
+// import { useState } from "react";
 
-  const response = await fetch(`https://bcsa-api.herokuapp.com/user/char/${characterId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type":"application/json",
-        "authorization":`Bearer ${token}`
-      }
-  })
-  try {
-      const data = await response.json();
-      console.log(data);
-      //TODO: Add 'class' field
-      const { characterName, characterClass, currency, def, exp, hp, level, items, atk, image } = data
-      setcharData({
-          atk,
-          characterName,
-          characterClass,
-          currency,
-          def,
-          exp,
-          hp,
-          items,
-          level,
-          image
-      });
-      return data;
-  } catch (err) {
-      console.log('Catch triggered')
-      console.log(err);
-  }
-};
+// const getCharacter = async ()=>{
+//   const token = localStorage.getItem("token");
+//   const characterId = localStorage.getItem("characterId");
+//   const obj = {};
+//   const response = await fetch(`https://bcsa-api.herokuapp.com/user/char/${characterId}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type":"application/json",
+//         "authorization":`Bearer ${token}`
+//       }
+//   })
+//   try {
+//       const data = await response.json();
+//       console.log(data);
+//       //TODO: Add 'class' field
+//       const { characterName, characterClass, currency, def, exp, hp, level, items, atk, image } = data
+//       obj = {
+//           atk,
+//           characterName,
+//           characterClass,
+//           currency,
+//           def,
+//           exp,
+//           hp,
+//           items,
+//           level,
+//           image
+//       };
+//       return data;
+//   } catch (err) {
+//       console.log('Catch triggered')
+//       console.log(err);
+//   }
+// };
 
-export default getCharacter;
+// export default getCharacter;
--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import assassin from '../assets/images/characters/assassin.png'
+import ranger from '../assets/images/characters/ranger.png'
+import routeMaster from '../assets/images/characters/routeMaster.png'
 export default function Loading() {
     return (
         <div className="pageContainer creationBg">
-            <h1 className='loading'>...Loading</h1>
+            <h1 className='loading'>...Loading
+            <img src={assassin} className="characterEmpty"></img>
+            <img src={ranger} className="characterEmpty"></img>
+            <img src={routeMaster} className="characterEmpty"></img>
+            </h1>
         </div>
     )
 }
