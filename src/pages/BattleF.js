1d60686cfd065be84c75acd3745fa51a00ce9a20
ZETA <ben.r.dominguez@gmail.com>
2022-03-16 03:25:32 -0700

    changed BattleJ2 music to hellscapejoe.mp3...

--- src/pages/BattleF.js
+++ src/pages/BattleF.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
-import song from '../assets/sounds/battle3.mp3'
+import song from '../assets/sounds/battle.mp3'
 import back from '../assets/images/back.png'
 import speakeron from '../assets/images/speaker-on.png'
 import speakeroff from '../assets/images/speaker-off.png'
--- src/pages/BattleJ2.js
+++ src/pages/BattleJ2.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
-import song from '../assets/sounds/battle.mp3'
+import song from '../assets/sounds/hellscapejoe.mp3'
 import back from '../assets/images/back.png'
 import speakeron from '../assets/images/speaker-on.png'
 import speakeroff from '../assets/images/speaker-off.png'
